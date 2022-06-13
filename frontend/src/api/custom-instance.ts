07d87be4690496d1caf3ee627746808483a8302b
siwakoti@usertechnologies.com <Govin>
2022-06-13 12:37:20 +0200

    fixed base url

--- frontend/src/api/custom-instance.ts
+++ frontend/src/api/custom-instance.ts
@@ -1,6 +1,6 @@
 import Axios, { AxiosRequestConfig } from 'axios';
 
-export const AXIOS_INSTANCE = Axios.create({ baseURL: 'http://127.0.0.1:8000' });
+export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.BACKEND_URL });
 
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
   return AXIOS_INSTANCE(config).then(({ data }) => data);
