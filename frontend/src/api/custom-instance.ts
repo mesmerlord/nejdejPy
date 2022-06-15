1ef4b0d6aa3aea4ad5dbc8886337c9946c596493
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-06-15 23:49:42 +0200

    fixed backend url

--- frontend/src/api/custom-instance.ts
+++ frontend/src/api/custom-instance.ts
@@ -1,9 +1,8 @@
 import Axios, { AxiosRequestConfig } from 'axios';
 
-export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.BACKEND_URL });
+export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.NEXT_BACKEND_URL });
 
-export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
-  return AXIOS_INSTANCE(config).then(({ data }) => data);
-};
+export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> =>
+  AXIOS_INSTANCE(config).then(({ data }) => data);
 
 export default customInstance;
