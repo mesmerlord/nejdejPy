573a3be2fab0c959e9401fb161fdbc9539b3d9a4
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-06-15 23:53:57 +0200

    fixed env variables

--- frontend/src/api/custom-instance.ts
+++ frontend/src/api/custom-instance.ts
@@ -1,6 +1,6 @@
 import Axios, { AxiosRequestConfig } from 'axios';
 
-export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.NEXT_BACKEND_URL });
+export const AXIOS_INSTANCE = Axios.create({ baseURL: process.env.NEXT_PUBLIC_BACKEND_URL });
 
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> =>
   AXIOS_INSTANCE(config).then(({ data }) => data);
