030d368236d9fc9a7c2c499c1a71496126182e7f
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-06-16 00:00:55 +0200

    fixed tests

--- nejdej/users/tests/test_swagger.py
+++ nejdej/users/tests/test_swagger.py
@@ -10,7 +10,8 @@ def test_swagger_accessible_by_admin(admin_client):
     assert response.status_code == 200
 
 
-def test_swagger_ui_not_accessible_by_normal_user(client):
+# def test_swagger_ui_not_accessible_by_normal_user(client):
+def test_swagger_ui_accessible_by_normal_user(client):
     url = reverse("api-docs")
     response = client.get(url)
     assert response.status_code == 403
